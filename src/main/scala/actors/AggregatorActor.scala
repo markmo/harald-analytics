0b562b1321f0de6211cf5c61703bdc949ce811ae
markmo <m4rkmo@gmail.com>
2017-06-19 08:44:54 +1000

    aggregator updates

--- src/main/scala/actors/AggregatorActor.scala
+++ src/main/scala/actors/AggregatorActor.scala
@@ -23,99 +23,126 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
   def receive = {
 
     case Start =>
-      val yearValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val qtrValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val monthValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val weekValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val dayValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val hourValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
-      val calendar = Calendar.getInstance
-      val systemMessages = spark.read.load(systemMessagesPath).as[SystemMessage]
-      systemMessages.foreach { msg =>
-        calendar.setTimeInMillis(msg.timestamp)
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val yearKey = new DateTime(year, 0, 1).getMillis
-        val qtrKey = startOfQuarter(year, month).getMillis
-        val monthKey = new DateTime(year, month, 1).getMillis
-        val weekKey = startOfWeek(year, month, day).getMillis
-        val dayKey = new DateTime(year, month, day).getMillis
-        val hourKey = new DateTime(year, month, day, hour).getMillis
-        val conversationId = msg.conversationId
-        val userId = msg.userId
-        val dimKey = DimensionKey(msg.resolutionStatus, msg.browser, msg.device, msg.operatingSystem)
-        val timestamp = msg.timestamp
-        updateTimeAggregate(yearValues, yearKey, userId, conversationId, dimKey, timestamp)
-        updateTimeAggregate(qtrValues, qtrKey, userId, conversationId, dimKey, timestamp)
-        updateTimeAggregate(monthValues, monthKey, userId, conversationId, dimKey, timestamp)
-        updateTimeAggregate(weekValues, weekKey, userId, conversationId, dimKey, timestamp)
-        updateTimeAggregate(dayValues, dayKey, userId, conversationId, dimKey, timestamp)
-        updateTimeAggregate(hourValues, hourKey, userId, conversationId, dimKey, timestamp)
-      }
-      val agg = Aggregate(
-        yearValues.toMap,
-        qtrValues.toMap,
-        monthValues.toMap,
-        weekValues.toMap,
-        dayValues.toMap,
-        hourValues.toMap
+      val Aggregate(year, qtr, month, week, day, hour) = calculateAggregate()
+
+      val previousTimeSteps = 30
+
+      val metricsMap: Map[String, Metrics] = Map(
+        "year" -> calculateMetrics("year", previousTimeSteps, year),
+        "qtr" -> calculateMetrics("qtr", previousTimeSteps, qtr),
+        "month" -> calculateMetrics("month", previousTimeSteps, month),
+        "week" -> calculateMetrics("week", previousTimeSteps, week),
+        "day" -> calculateMetrics("day", previousTimeSteps, day),
+        "hour" -> calculateMetrics("hour", previousTimeSteps, hour)
       )
-      // year
-      val yearAgg = agg.year
 
-      // take last 30 time steps
-      val items = yearAgg.keys.toList.sorted.takeRight(30)
 
-      // metrics
-      val conversationsPerUserAveragesYear: List[Double] = items map { key =>
-        val users = yearAgg(key).users
-        val k = users.size
-        val sum = users.values.map(_.size).sum
-        sum / k.toDouble
-      }
+  }
 
-      val conversationTotalsYear: List[Int] = items map { key =>
-        yearAgg(key).conversations.size
-      }
+  private def calculateMetrics(timePeriod: String,
+                               previousTimeSteps: Int,
+                               aggregate: Map[Long, TimeAggregate]): Metrics = {
 
-      val messageTotalsYear: List[Int] = items map { key =>
-        yearAgg(key).totalMessages
-      }
+    val items = aggregate.keys.toList.sorted.takeRight(previousTimeSteps)
 
-      val sessionLengthAveragesYear: List[Double] = items map { key =>
-        val users = yearAgg(key).users
-        val k = users.size
-        val sum = users.values.map { userConversations =>
-          val k = userConversations.size
-          val sum = userConversations.values.map { userConversation =>
-            val times = userConversation.sorted
-            val start = times.head
-            val end = times.reverse.head
-            end - start
-          }.sum
-          sum / k.toDouble
+    // metrics
+    val conversationsPerUserAverages: List[Double] = items map { key =>
+      val users = aggregate(key).users
+      val k = users.size
+      val sum = users.values.map(_.size).sum
+      sum / k.toDouble
+    }
+
+    val conversationTotals: List[Int] = items map { key =>
+      aggregate(key).conversations.size
+    }
+
+    val messageTotals: List[Int] = items map { key =>
+      aggregate(key).totalMessages
+    }
+
+    val sessionLengthAverages: List[Double] = items map { key =>
+      val users = aggregate(key).users
+      val k = users.size
+      val sum = users.values.map { userConversations =>
+        val k = userConversations.size
+        val sum = userConversations.values.map { userConversation =>
+          val times = userConversation.sorted
+          val start = times.head
+          val end = times.reverse.head
+          end - start
         }.sum
         sum / k.toDouble
-      }
+      }.sum
+      sum / k.toDouble
+    }
 
-      val stepsPerUserAveragesYear: List[Double] = items map { key =>
-        val conversations = yearAgg(key).conversations
-        val users = yearAgg(key).users
-        val k = users.size
-        val sum = users.values.map { userConversations =>
-          val k = userConversations.size
-          val sum = userConversations.keys.map { key =>
-            conversations(key)
-          }.sum
-          sum / k.toDouble
+    val stepsPerUserAverages: List[Double] = items map { key =>
+      val conversations = aggregate(key).conversations
+      val users = aggregate(key).users
+      val k = users.size
+      val sum = users.values.map { userConversations =>
+        val k = userConversations.size
+        val sum = userConversations.keys.map { key =>
+          conversations(key)
         }.sum
         sum / k.toDouble
-      }
+      }.sum
+      sum / k.toDouble
+    }
 
-      val userTotalsYear: List[Int] = items.map(key => yearAgg(key).users.size)
+    val userTotals: List[Int] = items.map(key => aggregate(key).users.size)
 
+    Metrics(timePeriod,
+      conversationsPerUserAverages,
+      conversationTotals,
+      messageTotals,
+      sessionLengthAverages,
+      stepsPerUserAverages,
+      userTotals
+    )
+  }
+
+  private def calculateAggregate(): Aggregate = {
+    val yearValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val qtrValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val monthValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val weekValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val dayValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val hourValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
+    val calendar = Calendar.getInstance
+    val systemMessages = spark.read.load(systemMessagesPath).as[SystemMessage]
+    systemMessages.foreach { msg =>
+      calendar.setTimeInMillis(msg.timestamp)
+      val year = calendar.get(Calendar.YEAR)
+      val month = calendar.get(Calendar.MONTH)
+      val day = calendar.get(Calendar.DAY_OF_MONTH)
+      val hour = calendar.get(Calendar.HOUR_OF_DAY)
+      val yearKey = new DateTime(year, 0, 1).getMillis
+      val qtrKey = startOfQuarter(year, month).getMillis
+      val monthKey = new DateTime(year, month, 1).getMillis
+      val weekKey = startOfWeek(year, month, day).getMillis
+      val dayKey = new DateTime(year, month, day).getMillis
+      val hourKey = new DateTime(year, month, day, hour).getMillis
+      val conversationId = msg.conversationId
+      val userId = msg.userId
+      val dimKey = DimensionKey(msg.resolutionStatus, msg.browser, msg.device, msg.operatingSystem)
+      val timestamp = msg.timestamp
+      updateTimeAggregate(yearValues, yearKey, userId, conversationId, dimKey, timestamp)
+      updateTimeAggregate(qtrValues, qtrKey, userId, conversationId, dimKey, timestamp)
+      updateTimeAggregate(monthValues, monthKey, userId, conversationId, dimKey, timestamp)
+      updateTimeAggregate(weekValues, weekKey, userId, conversationId, dimKey, timestamp)
+      updateTimeAggregate(dayValues, dayKey, userId, conversationId, dimKey, timestamp)
+      updateTimeAggregate(hourValues, hourKey, userId, conversationId, dimKey, timestamp)
+    }
+    Aggregate(
+      yearValues.toMap,
+      qtrValues.toMap,
+      monthValues.toMap,
+      weekValues.toMap,
+      dayValues.toMap,
+      hourValues.toMap
+    )
   }
 
   // TODO
@@ -161,6 +188,14 @@ object AggregatorActor {
   def props(config: Config, spark: SparkSession) = Props(new AggregatorActor(config, spark))
 
   case object Start
+
+  case class Metrics(timePeriod: String,
+                     conversationsPerUserAverages: List[Double],
+                     conversationTotals: List[Int],
+                     messageTotals: List[Int],
+                     sessionLengthAverages: List[Double],
+                     stepsPerUserAverages: List[Double],
+                     userTotals: List[Int])
 
   case class DimensionKey(resolutionStatus: String, browser: String, device: String, operatingSystem: String)
 
