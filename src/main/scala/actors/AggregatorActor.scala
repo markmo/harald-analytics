6e2918a81065627afeede2e50ab160c65f64995e
markmo <m4rkmo@gmail.com>
2017-06-21 09:27:45 +1000

    updates to aggregator - period changes

--- src/main/scala/actors/AggregatorActor.scala
+++ src/main/scala/actors/AggregatorActor.scala
@@ -20,37 +20,32 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
 
   val systemMessagesPath = config.getString("system-messages-path")
 
-  val resolvedDimKeys = dimKeys("resolved")
-  val unresolvedDimKeys = dimKeys("unresolved")
-  val abandonedDimKeys = dimKeys("abandoned")
-  val escalatedDimKeys = dimKeys("escalated")
-
   def receive = {
 
     case Start =>
-      val Aggregate(year, qtr, month, week, day, hour) = calculateAggregate()
+      val TimeAggregates(year, qtr, month, week, day, hour) = makeTimeAggregates()
 
       val previousTimeSteps = 30
 
-      val metricsMap: Map[String, Metrics] = Map(
-        "year" -> calculateMetrics("year", previousTimeSteps, year),
-        "qtr" -> calculateMetrics("qtr", previousTimeSteps, qtr),
-        "month" -> calculateMetrics("month", previousTimeSteps, month),
-        "week" -> calculateMetrics("week", previousTimeSteps, week),
-        "day" -> calculateMetrics("day", previousTimeSteps, day),
-        "hour" -> calculateMetrics("hour", previousTimeSteps, hour)
+      val timeMeasures: Map[String, Measures] = Map(
+        makeMeasures("year", previousTimeSteps, year),
+        makeMeasures("qtr", previousTimeSteps, qtr),
+        makeMeasures("month", previousTimeSteps, month),
+        makeMeasures("week", previousTimeSteps, week),
+        makeMeasures("day", previousTimeSteps, day),
+        makeMeasures("hour", previousTimeSteps, hour)
       )
-
 
   }
 
-  private def calculateMetrics(timePeriod: String,
-                               previousTimeSteps: Int,
-                               aggregate: Map[Long, TimeAggregate]): Metrics = {
+  def makeMeasures(timePeriod: String,
+                   previousTimeSteps: Int,
+                   aggregate: Map[Long, TimeAggregate]): (String, Measures) = {
 
     val items: List[Long] = aggregate.keys.toList.sorted.takeRight(previousTimeSteps)
 
-    // metrics
+    // Measures
+
     val conversationsPerUserAverages: List[Double] = items map { timeKey =>
       val users = aggregate(timeKey).users
       val k = users.size
@@ -98,39 +93,95 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
 
     val userTotals: List[Int] = items.map(timeKey => aggregate(timeKey).users.size)
 
-    val resolvedData = extractResolutionTotals(aggregate, items, resolvedDimKeys)
-
-    val unresolvedData = extractResolutionTotals(aggregate, items, unresolvedDimKeys)
-
-    val abandonedData = extractResolutionTotals(aggregate, items, abandonedDimKeys)
-
-    val escalatedData = extractResolutionTotals(aggregate, items, escalatedDimKeys)
-
-    Metrics(timePeriod,
-      conversationsPerUserAverages,
-      conversationTotals,
-      messageTotals,
-      sessionLengthAverages,
-      stepsPerUserAverages,
-      userTotals,
-      resolvedData,
-      unresolvedData,
-      abandonedData,
-      escalatedData
-    )
+    (timePeriod, Measures(timePeriod, Map(
+      makeDoubleMeasureAggregate("conversationsPerUserAverages", conversationsPerUserAverages),
+      makeIntMeasureAggregate("conversationTotals", conversationTotals),
+      makeIntMeasureAggregate("messageTotals", messageTotals),
+      makeDoubleMeasureAggregate("sessionLengthAverages", sessionLengthAverages),
+      makeDoubleMeasureAggregate("stepsPerUserAverages", stepsPerUserAverages),
+      makeIntMeasureAggregate("userTotals", userTotals),
+      makeResolutionAggregate("resolved", aggregate, items),
+      makeResolutionAggregate("unresolved", aggregate, items),
+      makeResolutionAggregate("abandoned", aggregate, items),
+      makeResolutionAggregate("escalated", aggregate, items)
+    )))
   }
 
-  private def extractResolutionTotals(aggregate: Map[Long, TimeAggregate],
-                                      items: List[Long],
-                                      dimKeys: Seq[Int]): List[Int] =
+  def makeIntMeasureAggregate(measureName: String, data: List[Int]): (String, MeasureAggregate) = {
+    val (sparklineData, thisPeriod, lastPeriod, change) = calculateIntChange(data)
+    (measureName, IntMeasureAggregate(
+      measureName,
+      data,
+      sparklineData,
+      thisPeriod,
+      lastPeriod,
+      change
+    ))
+  }
+
+  def calculateIntChange(data: List[Int]): (List[Int], Int, Int, Double) = {
+    val sparklineData: List[Int] = data.reverse.take(5)
+    val thisPeriod: Int = data.head
+    val lastPeriod: Int = data.slice(1, 1).head
+    val change: Double = if (lastPeriod == 0) {
+      1.0
+    } else {
+      (thisPeriod - lastPeriod) / lastPeriod.toDouble
+    }
+    (sparklineData, thisPeriod, lastPeriod, change)
+  }
+
+  def makeDoubleMeasureAggregate(measureName: String, data: List[Double]): (String, MeasureAggregate) = {
+    val (sparklineData, thisPeriod, lastPeriod, change) = calculateDoubleChange(data)
+    (measureName, DoubleMeasureAggregate(
+      measureName,
+      data,
+      sparklineData,
+      thisPeriod,
+      lastPeriod,
+      change
+    ))
+  }
+
+  def calculateDoubleChange(data: List[Double]): (List[Double], Double, Double, Double) = {
+    val sparklineData: List[Double] = data.reverse.take(5)
+    val thisPeriod: Double = data.head
+    val lastPeriod: Double = data.slice(1, 1).head
+    val change: Double = if (lastPeriod == 0) {
+      1.0
+    } else {
+      (thisPeriod - lastPeriod) / lastPeriod
+    }
+    (sparklineData, thisPeriod, lastPeriod, change)
+  }
+
+  def makeResolutionAggregate(resolutionStatus: String,
+                              aggregate: Map[Long, TimeAggregate],
+                              items: List[Long]): (String, MeasureAggregate) = {
+    val filterKeys: Seq[Int] = combineFilterKeys(resolutionStatus)
+    val data: List[Int] = extractDimTotals(aggregate, items, filterKeys)
+    val (sparklineData, totalThisPeriod, totalLastPeriod, changeThisPeriod) = calculateIntChange(data)
+    (resolutionStatus, IntMeasureAggregate(
+      resolutionStatus,
+      data,
+      sparklineData,
+      totalThisPeriod,
+      totalLastPeriod,
+      changeThisPeriod
+    ))
+  }
+
+  def extractDimTotals(aggregate: Map[Long, TimeAggregate],
+                       items: List[Long],
+                       filterKeys: Seq[Int]): List[Int] =
     items map { timeKey =>
       aggregate(timeKey).values.keys
-        .filter(resolvedDimKeys.contains)
+        .filter(filterKeys.contains)
         .map(aggregate(timeKey).values)
         .sum
     }
 
-  private def calculateAggregate(): Aggregate = {
+  def makeTimeAggregates(): TimeAggregates = {
     val yearValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
     val qtrValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
     val monthValues = mutable.HashMap[Long, TimeAggregate]().withDefaultValue(TimeAggregate())
@@ -162,7 +213,7 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
       updateTimeAggregate(dayValues, dayKey, userId, conversationId, dimKey, timestamp)
       updateTimeAggregate(hourValues, hourKey, userId, conversationId, dimKey, timestamp)
     }
-    Aggregate(
+    TimeAggregates(
       yearValues.toMap,
       qtrValues.toMap,
       monthValues.toMap,
@@ -174,12 +225,12 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
 
   // TODO
   // not a pure function
-  private def updateTimeAggregate(values: mutable.Map[Long, TimeAggregate],
-                                  key: Long,
-                                  userId: String,
-                                  conversationId: String,
-                                  dimKey: DimensionKey,
-                                  timestamp: Long): Unit = {
+  def updateTimeAggregate(values: mutable.Map[Long, TimeAggregate],
+                          key: Long,
+                          userId: String,
+                          conversationId: String,
+                          dimKey: DimensionKey,
+                          timestamp: Long): Unit = {
     val value = values(key)
     val conversations: Map[String, Int] = value.conversations
     val users: Map[String, Map[String, List[Long]]] = value.users
@@ -194,7 +245,7 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
     )
   }
 
-  private def startOfQuarter(year: Int, month: Int) =
+  def startOfQuarter(year: Int, month: Int) =
     if (month < 3) {
       new DateTime(year, 0, 1)
     } else if (month < 6) {
@@ -205,10 +256,10 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
       new DateTime(year, 9, 1)
     }
 
-  private def startOfWeek(year: Int, month: Int, day: Int) =
+  def startOfWeek(year: Int, month: Int, day: Int) =
     new DateTime(year, month, day).withDayOfWeek(DateTimeConstants.MONDAY)
 
-  private def dimKeys(resolutionStatus: String): Seq[Int] = {
+  def combineFilterKeys(resolutionStatus: String): Seq[Int] = {
     val undefined = ""
     cartesianProductOf(List(
       List(resolutionStatus),
@@ -220,7 +271,7 @@ class AggregatorActor(config: Config, spark: SparkSession) extends Actor with Ac
     }
   }
 
-  private def cartesianProductOf[T](xs: Traversable[Traversable[T]]): Seq[Seq[T]] =
+  def cartesianProductOf[T](xs: Traversable[Traversable[T]]): Seq[Seq[T]] =
     xs.foldLeft(Seq(Seq.empty[T])) { (x, y) =>
       for {
         a <- x.view
@@ -236,31 +287,37 @@ object AggregatorActor {
 
   case object Start
 
-  case class Metrics(timePeriod: String,
-                     conversationsPerUserAverages: List[Double],
-                     conversationTotals: List[Int],
-                     messageTotals: List[Int],
-                     sessionLengthAverages: List[Double],
-                     stepsPerUserAverages: List[Double],
-                     userTotals: List[Int],
-                     resolvedData: List[Int],
-                     unresolvedData: List[Int],
-                     abandonedData: List[Int],
-                     escalatedData: List[Int])
+  case class Measures(timePeriod: String, aggregates: Map[String, MeasureAggregate])
 
-  case class DimensionKey(resolutionStatus: String, browser: String, device: String, operatingSystem: String)
+  sealed trait MeasureAggregate
+
+  case class IntMeasureAggregate(measureName: String,
+                                 data: List[Int],
+                                 sparklineData: List[Int],
+                                 thisPeriod: Int,
+                                 lastPeriod: Int,
+                                 change: Double) extends MeasureAggregate
+
+  case class DoubleMeasureAggregate(measureName: String,
+                                    data: List[Double],
+                                    sparklineData: List[Double],
+                                    thisPeriod: Double,
+                                    lastPeriod: Double,
+                                    change: Double) extends MeasureAggregate
+
+  case class TimeAggregates(year: Map[Long, TimeAggregate],
+                            qtr: Map[Long, TimeAggregate],
+                            month: Map[Long, TimeAggregate],
+                            week: Map[Long, TimeAggregate],
+                            day: Map[Long, TimeAggregate],
+                            hour: Map[Long, TimeAggregate])
 
   case class TimeAggregate(values: Map[Int, Int] = Map().withDefaultValue(0),
                            totalMessages: Int = 0,
                            conversations: Map[String, Int] = Map().withDefaultValue(0),
                            users: Map[String, Map[String, List[Long]]] = Map().withDefaultValue(Map().withDefaultValue(Nil)))
 
-  case class Aggregate(year: Map[Long, TimeAggregate],
-                       qtr: Map[Long, TimeAggregate],
-                       month: Map[Long, TimeAggregate],
-                       week: Map[Long, TimeAggregate],
-                       day: Map[Long, TimeAggregate],
-                       hour: Map[Long, TimeAggregate])
+  case class DimensionKey(resolutionStatus: String, browser: String, device: String, operatingSystem: String)
 
   val browsers = List("chrome", "firefox", "ie", "opera")
   val devices = List("apple", "android")
